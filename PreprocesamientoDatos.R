e551b20314009e1c7a377082cd8a35a82ad1fcdb
Hamill Cavero <gillecave500@gmail.com>
2020-11-21 18:58:08 -0500

    Limpieza de valores que contienen muchos nulls y no aportan al Df

---
+++ PreprocesamientoDatos.R
@@ -0,0 +1,6 @@
+datos_violencia<-datos_violencia[,-c(16,17,18,19,42,43,44,30,28)]
+colnames(datos_violencia)
+View(datos_violencia)
+
+#BORRAR ID NIVEL EDUCATIVO,nivel educativo,ocupación,CUADRA
+#ID MATERIA Y MATERIA ID TIPO, TIPO,VIA,
