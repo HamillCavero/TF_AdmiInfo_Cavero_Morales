ceb5855ad3d4424e1be3221de7b9a7454c211057
Hamill Cavero <gillecave500@gmail.com>
2020-11-21 19:43:04 -0500

    edicion de la función

--- PreprocesamientoDatos.R
+++ PreprocesamientoDatos.R
@@ -1,8 +1,7 @@
-limpiar_violencia<-function(){
-  datos_violencia<-datos_violencia[,-c(16,17,18,19,42,43,44,30,28)]
-  colnames(datos_violencia)
-  View(datos_violencia)
+limpiar_violencia<-function(dv){
+  dv<-dv[,-c(16,17,18,19,42,43,44,30,28)]
+  return (dv)
 }
-
+datos_violencia<-limpiar_violencia(datos_violencia)
 #BORRAR ID NIVEL EDUCATIVO,nivel educativo,ocupación,CUADRA
 #ID MATERIA Y MATERIA ID TIPO, TIPO,VIA,
