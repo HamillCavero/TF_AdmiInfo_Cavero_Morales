9b438938fa414c3ca21332d403e77430e817d6a0
Hamill Cavero <gillecave500@gmail.com>
2020-11-21 19:12:48 -0500

    creación de save del df y no de la imagen en general

--- RecoleccionDatos.R
+++ RecoleccionDatos.R
@@ -1,7 +1,8 @@
+
 datos_violencia<-read.csv("datasets/SIDPOL_2017_Violencia_familiar.csv")
 View(datos_violencia)
 
 #Guardando Datos Backup#
-save.image("Datos_TF_Violencia_Admi.RData") 
+save(datos_violencia,file = "datasets/DF_violencia.Rda")
 
-load("dataset/Datos_TF_Violencia_Admi.RData")
+load("datasets/DF_violencia.Rda")
