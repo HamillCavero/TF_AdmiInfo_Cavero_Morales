1fa607167a9f878aa3c2e7ccab2432036f3e250d
aelvismorales <faviototio123@gmail.com>
2020-11-21 19:09:51 -0500

    Funcion Recoleccion o Lectura de datos

--- RecoleccionDatos.R
+++ RecoleccionDatos.R
@@ -1,7 +1,11 @@
-datos_violencia<-read.csv("datasets/SIDPOL_2017_Violencia_familiar.csv")
-View(datos_violencia)
-
+lectura<-function(direccion)
+  {
+    return(read.csv(direccion))
+  }
+datos_violencia<-lectura("datasets/SIDPOL_2017_Violencia_familiar.csv")
 #Guardando Datos Backup#
 save.image("Datos_TF_Violencia_Admi.RData") 
 
 load("dataset/Datos_TF_Violencia_Admi.RData")
+
+
