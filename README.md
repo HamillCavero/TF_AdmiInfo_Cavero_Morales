4c4eafe312c37c1f1c40bce63e3f1e15152809f1
Hamill Cavero <gillecave500@gmail.com>
2020-11-22 19:00:52 -0500

    Creación de graficos.

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Trabajo Final de Administración de la información de Cavero Hamill y Morales E
   * Creación de save del df y no de la imagen en general
   * Combinación?
   * Agregando en Preprocesamiento modicación de fechas
+  * Gráficos de los querys 2 al 8, sujetos a cambios.
 This is a line from RStudio
 
---
+++ graficos.R
@@ -0,0 +1,30 @@
+library(corrplot)
+library(ggplot2)
+
+#grafico del q2
+qplot(x=Modalidad,y=Cantidad_Casos,data = q2,geom = "col")+aes(fill= Modalidad)
+
+#grafico del q3
+qplot(x=Total,y=DPTO_CIA,data = q3,geom = "col")+aes(fill= DPTO_CIA)
+
+#grafico del q4
+qplot(x=SIT_PERSONA,y=Total,data = q4%>%slice(1:5),geom = "col")+aes(fill= SIT_PERSONA)
+
+#grafico del q5
+qplot(x=pais_natal  ,y=Total,data = q5%>%slice(1:5),geom = "col")+aes(fill= pais_natal)
+
+#grafico del q6
+qplot(x=EDAD   ,y=Total,data = q6,geom = "col")+aes(fill= EDAD )
+
+#grafico del q7
+qplot(x=EDAD   ,y=Total,data = q7,geom = "col")+aes(fill= EDAD )
+ggplot(data = q7, aes(x="", y=q7$Total, fill=q7$SEXO)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
+
+#grafico del q8
+qplot(x=q8$DIST_CIA   ,y=Total,data = q8,geom = "col")+aes(fill= q8$DIST_CIA )
+
+
+RColorBrewer::display.brewer.all() 
+
