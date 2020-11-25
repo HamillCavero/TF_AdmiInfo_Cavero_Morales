0677ca8d518c28a771d8b27745fc84dbafb9e29e
aelvismorales <faviototio123@gmail.com>
2020-11-25 09:02:57 -0500

    .

--- GeneralScript.R
+++ GeneralScript.R
@@ -3,4 +3,7 @@ source("RecoleccionDatos.R")
 source("PreprocesamientoDatos.R")
 source("Transformacion.R")
 
+#Guardando Datos Backup#
+save.image("datasets/BackupDatosViolencia.Rda")
 
+load("datasets/BackupDatosViolencia.Rda")
