a57fdef3ea33e5094b5ec5d1c3d631b8c63c58d4
Hamill Cavero <gillecave500@gmail.com>
2020-11-27 20:33:15 -0500

    comentando los graficos en el codigo

--- TrabajoFinalAdminInfo_Cavero_Morales/Codigo/vinito_barato.R
+++ TrabajoFinalAdminInfo_Cavero_Morales/Codigo/vinito_barato.R
@@ -80,11 +80,11 @@ bbbb <- kmeans(semillas[,c(1,6)], 3, iter.max = 1000, nstart = 10)
 #dataset$cluster <- kmeans$cluster
 semillas2<-semillas
 semillas2$cluster<-bbbb$cluster
-ggplot() + geom_point(aes(x = semillas2$Area, y = semillas2$Coeficiente_de_Asimetria, color = cluster), data = semillas2, size = 2) +
-  scale_colour_gradientn(colours=rainbow(4)) +
-  geom_point(aes(x = bbbb$centers[, 1], y = bbbb$centers[, 2]), color = 'black', size = 3) + 
-  ggtitle('Clusters de Datos con k = 3 / K-Medios') + 
-  xlab('X') + ylab('Y')
+# ggplot() + geom_point(aes(x = semillas2$Area, y = semillas2$Coeficiente_de_Asimetria, color = cluster), data = semillas2, size = 2) +
+#   scale_colour_gradientn(colours=rainbow(4)) +
+#   geom_point(aes(x = bbbb$centers[, 1], y = bbbb$centers[, 2]), color = 'black', size = 3) + 
+#   ggtitle('Clusters de Datos con k = 3 / K-Medios') + 
+#   xlab('X') + ylab('Y')
 ####
 
 semillas1 <- semillas1 %>% mutate(cluster = aaaa$cluster)
@@ -92,35 +92,35 @@ semillas1 <- semillas1 %>% mutate(cluster = as.factor(cluster),
                           #grupo   = as.factor(grupo))
                           grupo   = 3)
 
-ggplot(data = semillas1, aes(x = semillas1$Perimetro, y = semillas1$Coeficiente_de_Asimetria, color = semillas1$cluster)) +
-  geom_text(aes(label = cluster), size = 5) +
-  theme_bw() +
-  theme(legend.position = "none")
+# ggplot(data = semillas1, aes(x = semillas1$Perimetro, y = semillas1$Coeficiente_de_Asimetria, color = semillas1$cluster)) +
+#   geom_text(aes(label = cluster), size = 5) +
+#   theme_bw() +
+#   theme(legend.position = "none")
 
 #colnames(datos_violencia)
 
 
-plot(semillas1$largo,semillas1$ancho)
+#plot(semillas1$largo,semillas1$ancho)
 reg_semillas1<-abline(lm(semillas1$ancho~semillas1$largo),col="red")
-ggplot(data =semillas1, aes(largo,ancho))+ geom_point() +   geom_smooth()
-ggplot(data =semillas1, aes(largo,ancho))+ geom_point() +   geom_smooth(method = "lm")
+#ggplot(data =semillas1, aes(largo,ancho))+ geom_point() +   geom_smooth()
+#ggplot(data =semillas1, aes(largo,ancho))+ geom_point() +   geom_smooth(method = "lm")
 
-plot(semillas1$Perimetro,semillas1$ancho)
-reg_semillas1<-abline(lm(semillas1$ancho~semillas1$Perimetro),col="red")
+#plot(semillas1$Perimetro,semillas1$ancho)
+#reg_semillas1<-abline(lm(semillas1$ancho~semillas1$Perimetro),col="red")
 
-ggplot(data =semillas1, aes(Perimetro,ancho))+ geom_point() +   geom_smooth()
+#ggplot(data =semillas1, aes(Perimetro,ancho))+ geom_point() +   geom_smooth()
 
 
-plot(semillas1$Perimetro,semillas1$Area)
-reg_smlls1<-lm(semillas1$Area~semillas1$Perimetro)
-reg_semillas1<-abline(lm(semillas1$Area~semillas1$Perimetro),col="red")
+#plot(semillas1$Perimetro,semillas1$Area)
+#reg_smlls1<-lm(semillas1$Area~semillas1$Perimetro)
+#reg_semillas1<-abline(lm(semillas1$Area~semillas1$Perimetro),col="red")
 
-ggplot(data =semillas1, aes(Perimetro,Area))+ geom_point() +   geom_smooth()
+#ggplot(data =semillas1, aes(Perimetro,Area))+ geom_point() +   geom_smooth()
 
 ########regresión multivariable
 #f=lm(mpg~disp+hp+drat+wt,data = mtcars)
 
 regre_multi_semillas<-lm(data = semillas,semillas$Area~semillas$Perimetro+semillas$largo+semillas$ancho)
-ggplot(data=regre_multi_semillas,aes(x = regre_multi_semillas$fitted.values,y = regre_multi_semillas$residuals))+
-  geom_point() + geom_smooth()
+# ggplot(data=regre_multi_semillas,aes(x = regre_multi_semillas$fitted.values,y = regre_multi_semillas$residuals))+
+#   geom_point() + geom_smooth()
 
